75e97df6826e6b156637b0fba42d37a5addf61f9
kokocan12 <49009864+kokocan12@users.noreply.github.com>
2020-07-16 21:06:30 +0900

    spring-mvc-demo

---
+++ src/com/luv2code/springdemo/mvc/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+}
